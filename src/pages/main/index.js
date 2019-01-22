e8978ddec0750674f20424aa900488e39e88eb91
Thiago Maturana <thiagomatu@gmail.com>
2019-01-21 22:30:11 -0200

    added a simple form in a statefull component

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,5 +1,34 @@
-import React from 'react';
+/* eslint-disable react/jsx-one-expression-per-line */
+/* eslint-disable react/destructuring-assignment */
+import React, { Component, Fragment } from 'react';
 
-const Main = () => <h1>Main</h1>;
+export default class Main extends Component {
+  state = {
+    repositoryInput: '',
+  };
 
-export default Main;
+  render() {
+    return (
+      <Fragment>
+        <form onSubmit={this.handleAddRepository}>
+          <input
+            type="text"
+            placeholder="User/Repository"
+            value={this.state.repositoryInput}
+            onChange={e => this.setState({ repositoryInput: e.target.value })}
+          />
+          <button type="submit">Add</button>
+        </form>
+
+        <ul>
+          <li>
+            <p>
+              <strong>facebook/react</strong> (react description)
+            </p>
+            <a href="http://github.com/facebook/react">Access</a>
+          </li>
+        </ul>
+      </Fragment>
+    );
+  }
+}
